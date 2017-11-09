8df8022d6e49b919cedacc842a124d0bc53ce089
Matthew Walcher <m.walcher@arcane.ws>
2017-11-09 10:23:10 -0500

    added stylesheet to register page

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -5,6 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Registration Form</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
     </head>
     <body>
         <h1>Registration Form</h1>
